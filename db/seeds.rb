4c24db89b306f1d3171b7c28eb973efb22aea76c
Cedric Dlh <cedricdlh@gmail.com>
2019-04-15 00:02:45 +0200

    various little changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,4 +19,16 @@ puts "events cleaned. Creating new..."
             # buddy: false, email: "a@a.at", password: "12345678")
 
 
-Event.create(name: "Pablo & friends", date: "11/05/2019", time: "21H", tickets_available: 25, price: 2000)
+Event.create(name: "Formula 1 : UK GP", date: "14/07/2019", time: "14H", tickets_available: 250, price: 200000)
+
+Event.create(name: "Champions League Final", date: "01/06/2019", time: "20H", tickets_available: 1000, price: 10000)
+
+Event.create(name: "Muse in Cracow", date: "22/06/2019", time: "18H", tickets_available: 1500, price: 150000)
+
+Event.create(name: "Sziget Festival", date: "07/08/2019", time: "16H", tickets_available: 20000, price: 500000)
+
+Event.create(name: "Tomorrowland", date: "19/07/2019", time: "12H", tickets_available: 0, price: 20000000000)
+
+Event.create(name: "French Open Tennis", date: "26/05/2019", time: "13H", tickets_available: 25, price: 1500000)
+
+puts "All done you're good to go with 5 cool events"
